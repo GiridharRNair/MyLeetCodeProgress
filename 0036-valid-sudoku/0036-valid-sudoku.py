64996e4d9ebb2a65e30fe4e9db602256d22025cf
Giridhar Nair <49298134+GiridharRNair@users.noreply.github.com>
2023-08-15 15:58:34 -0500

    Time: 102 ms (85.19%), Space: 16.2 MB (80.95%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -10,9 +10,9 @@ class Solution:
                     continue
                 if (board[r][c] in rows[r] 
                     or board[r][c] in cols[c] 
-                    or board[r][c] in squares[3 * (r // 3) + c // 3]):
+                    or board[r][c] in squares[3 * (r // 3) + (c // 3)]):
                     return False
                 rows[r].append(board[r][c])
                 cols[c].append(board[r][c])
-                squares[3 * (r // 3) + c // 3].append(board[r][c])
+                squares[3 * (r // 3) + (c // 3)].append(board[r][c])
         return True
