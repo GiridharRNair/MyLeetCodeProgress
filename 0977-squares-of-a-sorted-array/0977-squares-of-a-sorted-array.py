a4d52ef72360715f61a0b51dc61642cb601dccc4
Giridhar Nair <49298134+GiridharRNair@users.noreply.github.com>
2023-09-25 21:44:31 -0500

    Time: 188 ms (67.61%), Space: 18.3 MB (90.66%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -2,5 +2,5 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         for idx in range(len(nums)):
             num = nums[idx] 
-            nums[idx] = num * num
+            nums[idx] = num **2
         return sorted(nums)
